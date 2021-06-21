23a2923f66cf58dcbe26a7e5abc9acd0645822d7
Vishal Ingle <vishalingle782@gmail.com>
2021-06-21 17:15:03 +0530

    databse connection

---
+++ api/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$connect = mysqli_connect("localhost","root","","voting") or die("connection faild");
+
+if ($connect)
+ {
+    echo "connected";
+
+}
+else{
+    echo "not connected";
+
+}
+?>
