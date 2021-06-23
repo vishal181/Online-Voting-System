590f490927f44f2d282dcfe83bb4658870292029
Vishal Ingle <vishalingle782@gmail.com>
2021-06-23 20:17:57 +0530

    solved buggs

--- api/register.php
+++ api/register.php
@@ -13,7 +13,7 @@ $role = $_POST['role'];
 
 if ($password == $cpassword) {
     move_uploaded_file($tmp_name, "../uploads/$image");
-    $insert = mysqli_query($connect, "INSERT INTO user (name,mobile,address,password,photo,role,status,votes) VALUES ('$name','$mobile','$password','$address','$image','$role',0,0)");
+    $insert = mysqli_query($connect, "INSERT INTO user (name,mobile,address,password,photo,role,status,votes) VALUES ('$name','$mobile','$address','$password','$image','$role',0,0)");
 
     if ($insert)
      {
