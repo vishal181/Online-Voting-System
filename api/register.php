8f97a2104ab960bb71ceff1af16555e63c3ac843
Vishal Ingle <vishalingle782@gmail.com>
2021-06-23 19:57:49 +0530

    data taken and added to database

---
+++ api/register.php
@@ -0,0 +1,53 @@
+<?php
+
+include("connect.php");
+
+$name = $_POST['name'];
+$mobile = $_POST['mobile'];
+$password = $_POST['password'];
+$cpassword = $_POST['cpassword'];
+$address = $_POST['address'];
+$image = $_FILES['name']['photo'];
+$tmp_name = $_FILES['tmp_name']['photo'];
+$role = $_POST['role'];
+
+if ($password == $cpassword) {
+    move_uploaded_file($tmp_name, "../uploads/$image");
+    $insert = mysqli_query($connect, "INSERT INTO user (name,mobile,address,password,photo,role,status,votes) VALUES ('$name','$mobile','$password','$address','$image','$role',0,0)");
+
+    if ($insert)
+     {
+        echo '
+        <script>
+        
+        alert("Registration Sucessful!");
+        window.location= "../";
+    
+        </script>
+    
+        ';
+    } else 
+    {
+        echo '
+        <script>
+        
+        alert("Error occured!");
+        window.location= "../registrationfiles/registration.html";
+    
+        </script>
+    
+        ';
+    }
+} 
+else
+ {
+    echo '
+    <script>
+    
+    alert("Password and confirm Password does not match!");
+    window.location= "../registrationfiles/registration.html";
+
+    </script>
+
+    ';
+}
