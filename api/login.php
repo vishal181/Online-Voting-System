748b8804399a2ca91a7f57ad6c2ec6e0851cef98
Vishal Ingle <vishalingle782@gmail.com>
2021-06-29 00:12:36 +0530

    asap

--- api/login.php
+++ api/login.php
@@ -2,30 +2,29 @@
 session_start();
 include("connect.php");
 
-$mobile=$_POST['mobile'];
-$password=$_POST['password'];
-$role=$_POST['role'];
+$mobile = $_POST['mobile'];
+$password = $_POST['password'];
+$role = $_POST['role'];
 
-$check=mysqli_query($connect,"SELECT * FROM user WHERE mobile='$mobile' AND password='$password' AND role='$role' ");
+$check = mysqli_query($connect, "SELECT * FROM user WHERE mobile='$mobile' AND password='$password' AND role='$role' ");
 
-if (mysqli_num_rows($check)>0)
- {
-    $userdata=mysqli_fetch_array($check);
-    $group=mysqli_query($connect,"SELECT * FROM user WHERE role=2");
-    $groupdata=mysqli_fetch_all($group,MYSQLI_ASSOC);
-    $_SESSION['userdata']=$userdata;
-    $_SESSION['groupdata']=$groupdata;
-    echo '
+if (mysqli_num_rows($check) > 0) {
+      $userdata = mysqli_fetch_array($check);
+      $groups = mysqli_query($connect, "SELECT * FROM user WHERE role=2");
+      $groupsdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
+      $_SESSION['userdata'] = $userdata;
+      $_SESSION['groupsdata'] = $groupsdata;
+      echo '
     <script>
     window.location="../registrationfiles/dashboard.php";
     </script>
           ';
-}
-else{
-    echo '
+} else {
+      echo '
     <script>
     alert("invalid credintials!");
     window.location="../";
     </script>
           ';
 }
+?>
