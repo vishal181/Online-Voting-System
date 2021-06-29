04a9344d3285132d5dad6e3d638bc6fae05cdbd2
Vishal Ingle <vishalingle782@gmail.com>
2021-06-29 21:50:42 +0530

    buggsfixx

--- api/register.php
+++ api/register.php
@@ -1,7 +1,6 @@
-<?php
-
 include("connect.php");
 
+<?php
 $name = $_POST['name'];
 $mobile = $_POST['mobile'];
 $password = $_POST['password'];
@@ -15,8 +14,7 @@ if ($password == $cpassword) {
     move_uploaded_file($tmp_name, "../uploads/$image");
     $insert = mysqli_query($connect, "INSERT INTO user (name,mobile,address,password,photo,role,status,votes) VALUES ('$name','$mobile','$address','$password','$image','$role',0,0)");
 
-    if ($insert)
-     {
+    if ($insert) {
         echo '
         <script>
         
@@ -26,8 +24,7 @@ if ($password == $cpassword) {
         </script>
     
         ';
-    } else 
-    {
+    } else {
         echo '
         <script>
         
@@ -38,9 +35,7 @@ if ($password == $cpassword) {
     
         ';
     }
-} 
-else
- {
+} else {
     echo '
     <script>
     
@@ -51,3 +46,4 @@ else
 
     ';
 }
+?>
