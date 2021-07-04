1ed6657766dff2dcd378f05eb5ffe819e6b8085a
Vishal Ingle <vishalingle782@gmail.com>
2021-07-05 00:29:28 +0530

    new change

--- registrationfiles/dashboard.php
+++ registrationfiles/dashboard.php
@@ -40,13 +40,10 @@ $groupsdata = $_SESSION['groupsdata'];
             border-radius: 5px;
             background-color: #3498db;
             color: white;
-            float: left;
-            padding: 5px;
-            font-size: 15px;
-            border-radius: 5px;
-            background-color: #3498db;
-            color: white;
             float: right;
+            padding: 5px;
+
+
         }
 
         #profile {
@@ -66,9 +63,7 @@ $groupsdata = $_SESSION['groupsdata'];
             float: right;
 
         }
-        #mainpanal{
-            padding: 10px;
-        }
+
 
         #votebtn {
             padding: 5px;
@@ -91,45 +86,45 @@ $groupsdata = $_SESSION['groupsdata'];
         </center>
         <hr>
 
-        
 
 
-            <div id="profile">
-                <center> <img src="../uploads/<?php echo $userdata['photo'] ?>" height="100" width="100"></center> <br>
-                <b>Name:</b> <?php echo $userdata['name']   ?> <br><br>
-                <b>Mobile:</b><?php echo $userdata['mobile']   ?><br><br>
-                <b>Address:</b><?php echo $userdata['address']   ?><br><br>
-                <b>Status:</b><?php echo $userdata['status']   ?><br><br>
 
-            </div>
+        <div id="profile">
+            <center> <img src="../uploads/<?php echo $userdata['photo'] ?>" height="100" width="100"></center> <br>
+            <b>Name:</b> <?php echo $userdata['name']   ?> <br><br>
+            <b>Mobile:</b><?php echo $userdata['mobile']   ?><br><br>
+            <b>Address:</b><?php echo $userdata['address']   ?><br><br>
+            <b>Status:</b><?php echo $userdata['status']   ?><br><br>
 
-            <div id="group">
-                <?php
-                if ($_SESSION['groupsdata']) {
-                    for ($i = 0; $i < count($groupsdata); $i++) {
-                ?>
-                        <div>
-                            <img src="../uploads/<?php echo  $groupsdata[$i]['photo'] ?>" height="100" width="100">
-                            <b>Group Name: </b><?php echo $groupsdata[$i]['name'] ?><br> <br>
-                            <b>Votes: </b><?php echo $groupsdata[$i]['votes'] ?><br> <br>
-                            <form action="">
-                                <input type="hidden" name="gvotes" value="<?php echo $groupsdata[$i]['votes'] ?>">
-                                <input type="submit" name="votebtn" value="vote" id="votebtn">
-                            </form>
-                        </div>
-                        <hr>
+        </div>
 
-                <?php
+        <div id="group">
+            <?php
+            if ($_SESSION['groupsdata']) {
+                for ($i = 0; $i < count($groupsdata); $i++) {
+            ?>
+                    <div>
+                        <img src="../uploads/<?php echo  $groupsdata[$i]['photo'] ?>" height="100" width="100">
+                        <b>Group Name: </b><?php echo $groupsdata[$i]['name'] ?><br> <br>
+                        <b>Votes: </b><?php echo $groupsdata[$i]['votes'] ?><br> <br>
+                        <form action="">
+                            <input type="hidden" name="gvotes" value="<?php echo $groupsdata[$i]['votes'] ?>">
+                            <input type="submit" name="votebtn" value="vote" id="votebtn">
+                        </form>
+                    </div>
+                    <hr>
 
-                    }
-                } else {
+            <?php
+
                 }
+            } else {
+            }
 
 
 
-                ?>
-            </div>
-        
+            ?>
+        </div>
+
 
     </div>
 
